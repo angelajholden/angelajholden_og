6c147bfa8eacc375b2a7eea2637f1b37a9699c63
Angela J. Holden <angelajholden@gmail.com>
2014-06-01 07:17:51 -0700

    adding a page template

---
+++ page.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+
+Template Name: Page w/Sidebar
+
+
+*/
+get_header(); the_post(); ?>
+
+  <div class="blogSpace"></div>
+
+    <div class="blogWrap">
+
+      <article class="singlePost">
+
+        <header>
+          <h1><?php the_title(); ?></h1>
+        </header>
+
+          <?php if ( has_post_thumbnail() ) { ?>
+            <figure>
+              <?php the_post_thumbnail('full'); ?>
+              <figcaption><?php the_post_thumbnail_caption(); ?></figcaption>
+            </figure>
+          <?php } ?>
+
+          <?php the_content(); ?>
+
+        <?php edit_post_link('Edit'); ?>
+
+      </article>
+
+    </div><?php //Blog Wrap ?>
+
+  <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
