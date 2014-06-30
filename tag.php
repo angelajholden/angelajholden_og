70bafeb5180bfcb0656c300f4e5df06121153854
Angela J. Holden <angelajholden@gmail.com>
2014-06-29 20:22:05 -0700

    span to capitalize tag title, moving post thumbnail above meta, link for main archive

--- tag.php
+++ tag.php
@@ -5,8 +5,8 @@
 ?>
 
   <header>
-    <h1>Tag: <?php single_tag_title(); ?></h1>
-    <p>These are all of the posts about <?php single_cat_title(); ?>.</p>
+    <h1><span><?php single_tag_title(); ?></span></h1>
+    <p>These are all of the posts tagged ‘<?php single_cat_title(); ?>’</p>
   </header>
 
   	<div class="blogWrap clearfix">
@@ -17,11 +17,13 @@
 
         <h2 class="archiveTitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-          <p>Posted in <?php the_category(','); ?> on <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F j, Y'); ?></time></p>
+        <?php if ( has_post_thumbnail() ) { ?>
+          <a href="<?php the_permalink(); ?>">
+            <figure><?php the_post_thumbnail('large'); ?></figure>
+          </a>
+        <?php } ?>
 
-          <?php if ( has_post_thumbnail() ) { ?>
-            <figure><?php the_post_thumbnail('thumbnail'); ?></figure>
-          <?php } ?>
+          <p>Posted in <?php the_category(','); ?> on <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F j, Y'); ?></time></p>
 
           <p class="archiveExcerpt">
             <?php
@@ -35,6 +37,8 @@
 
       <?php endwhile; else: ?>
       <?php endif; ?>
+
+      <div style="clear:both;"><a href="<?php bloginfo('url') ?>/main-archive/">View All Articles</a></div>
 
   	</div>
 
