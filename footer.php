b744376c7c4e4e830faa28c9675ae4ec45ca3c70
Angela J. Holden <angelajholden@gmail.com>
2014-06-29 19:41:11 -0700

    minor edits to about me

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
       	<img src="<?php bloginfo('template_url'); ?>/images/angela.jpg" alt="About Angela J. Holden Website Design" title="Angela J. Holden">
       	<p>My name is Angela Holden and I’m a freelance web designer and technical writer from Minneapolis, MN, now living in San Diego, CA. I love WordPress, content, color, typography, and providing great user experiences. I’ve been designing websites for more than four years and I love every minute that I spend doing it. In 2009 I built my first website, and in 2013 I earned a Master’s Degree in Technical Communication from Metropolitan State University.</p>
 
-        <p>I work from my home in San Diego, and have clients in the U.S. and overseas. For more information about what I can do, please contact me for a consultation.</p>
+        <p>I work from my home in San Diego and I have clients in the U.S. and overseas. For more information about what I can do, please <a href="<?php bloginfo('url'); ?>/contact/">contact</a> me for a consultation.</p>
 
         <p>Cheers!</p>
         <div class="signature"><?php include('inc/signature.svg'); ?></div>
