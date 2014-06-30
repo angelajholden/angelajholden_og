9783534660b4a6dbe7f7411f756a4f88699a7b76
Angela J. Holden <angelajholden@gmail.com>
2014-06-29 19:36:54 -0700

    improvements to snippets archive view

--- archive-snippet.php
+++ archive-snippet.php
@@ -8,6 +8,7 @@ get_header(); ?>
 
   <header>
     <h1>Snippets</h1>
+    <p>These are some of the snippets that I've collected, modified, and use frequently in my projects. When possible, an attribution for the original source is provided.</p>
   </header>
 
   	<div class="blogWrap clearfix">
@@ -23,27 +24,29 @@ get_header(); ?>
 
   		<article class="archivePost clearfix">
 
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2 class="archiveTitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
           <p class="archiveTime">Posted on <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F j, Y'); ?></time></p>
 
           <div class="theTags"><?php the_terms( $snippet->ID, 'keyword', 'Keywords: ', ' <span>•</span> ' ); ?></div>
 
           <?php if ( has_post_thumbnail() ) { ?>
-            <figure class="snippetImage"><?php the_post_thumbnail('full'); ?></figure>
+            <a href="<?php the_permalink(); ?>">
+              <figure class="snippetImage"><?php the_post_thumbnail('full'); ?></figure>
+            </a>
           <?php } ?>
 
           <p class="archiveExcerpt">
             <?php
               $content = get_the_content();
-              $trimmed_content = wp_trim_words( $content, 20, '... <a class="archiveLink" href="'. get_permalink() .'">Read More</a>' );
+              $trimmed_content = wp_trim_words( $content, 20, '...<br /><a class="archiveLink" href="'. get_permalink() .'">Read More</a>' );
               echo $trimmed_content;
             ?>
           </p>
 
         </article>
 
-      <?php  endwhile; ?>
+      <?php endwhile; ?>
       <?php wp_reset_postdata(); ?>
 
   	</div>
