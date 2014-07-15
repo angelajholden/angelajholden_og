72d0fd403dcc32ce7b90273b90fb49c2ecf46ab1
Angela J. Holden <angelajholden@gmail.com>
2014-07-14 19:36:35 -0700

    class name for the title, adding search, adding class to wrap

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 
   <div class="blogSpace"></div>
 
-    <div class="blogWrap">
+  <?php include('search.php'); ?>
+
+    <div class="blogWrap frontWrap">
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           
         <article class="archivePost clearfix">
 
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h2 class="archiveTitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
           <?php if ( has_post_thumbnail() ) { ?>
             <a href="<?php the_permalink(); ?>">
