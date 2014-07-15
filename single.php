1a12609ca4d952e601327c917e3ae6d616110d95
Angela J. Holden <angelajholden@gmail.com>
2014-07-14 20:01:13 -0700

    conditional statement for a video first, feat. image last

--- single.php
+++ single.php
@@ -12,7 +12,12 @@
           <h1><?php the_title(); ?></h1>
         </header>
 
-        <?php if ( has_post_thumbnail() ) { ?>
+        <?php $video = get_post_meta( get_the_ID(), 'video_url', true );
+            if( ! empty( $video ) ) { ?>
+            <figure class="iframe">
+              <?php echo parse_youtube_url($video,'embed'); ?>
+            </figure>
+        <?php } else ( has_post_thumbnail() ) { ?>
           <figure>
             <?php the_post_thumbnail('full'); ?>
             <figcaption><?php the_post_thumbnail_caption(); ?></figcaption>
@@ -22,14 +27,6 @@
         <p>Posted in <?php the_category(','); ?> on <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F j, Y'); ?></time></p>
 
          <div class="theTags"><?php the_tags( 'Tagged: ', ' <span>•</span> ', '' ); ?></div>
-
-          <?php 
-            $video = get_post_meta( get_the_ID(), 'video_url', true );
-            if( ! empty( $video ) ) { ?>
-            <figure class="iframe">
-              <?php echo parse_youtube_url($video,'embed'); ?>
-            </figure>
-          <?php } ?>
 
           <?php the_content(); ?>
 
