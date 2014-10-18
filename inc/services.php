46efe86a2c4b94d35b0a64fe5cb76055120cfe65
Angela J. Holden <angelajholden@gmail.com>
2014-10-18 06:27:37 -0700

    renamed to .php

---
+++ inc/services.php
@@ -0,0 +1,53 @@
+<h2 class="servicesTitle">My skillset is versatile and I get hired to work on many different types of projects.</h2>
+
+	<div class="ajh-skillset clearfix">
+		<div><span class="fa fa-code"></span><h3>Site Edits</h3>Modify or add functionality to an existing website.</div>
+		<div><span class="fa fa-paint-brush"></span><h3>Styling</h3>Use CSS &amp; JQuery to style a website’s markup.</div>
+		<div><span class="fa fa-desktop"></span><h3>Design</h3>Create website mockups in Photoshop.</div>
+		<div><span class="fa fa-pencil"></span><h3>Information Architecture</h3>Content inventory, audit, and strategy.</div>
+		<div><span class="fa fa-wordpress"></span><h3>WordPress Setup</h3>Install WordPress on your domain.</div>
+		<div><span class="fa fa-mobile"></span><h3>App Landing Page</h3>One page scrolling website to support an app.</div>
+	</div>
+
+	<div class="srvc usd">
+		<h2><span class="fa fa-usd"></span>Pricing</h2>
+		<p>My hourly rate is $40, but I have special pricing for WordPress installation, setup and theme customization. For custom work I estimate the amount of time I think it will take to complete a project and quote a fixed price. My terms are 50% to begin work, and the remaining amount due upon completion.</p>
+		<p>If you want mockups for custom work prior to formalizing an arrangement, I charge a flat fee of $250 which I'll credit towards the total cost of the project.</p>
+	</div>
+
+	<div class="srvc clock">
+		<h2><span class="fa fa-clock-o"></span>Turn Around Time</h2>
+		<p>For most projects my turn around time is 7-10 days, but hopefully much sooner. For a custom WordPress website the process generally takes 3-4 weeks.</p>
+	</div>
+
+	<div class="srvc picture">
+		<h2><span class="fa fa-picture-o"></span>Images</h2>
+		<p>For all projects I provide 3 complimentary, royalty-free, high resolution images from <a href="http://mbsy.co/9clQc">Dollar Photo Club</a> for use on your website. Additional images are just $1.</p>
+	</div>
+
+	<div class="srvc users">
+		<h2><span class="fa fa-users"></span>Social Media</h2>
+		<p>I can help you implement a social media strategy that's integrated with your website, and is part of an overall content strategy. I believe this is an important aspect to the success of your endeavour.</p>
+	</div>
+
+	<div class="srvc tint">
+		<h2><span class="fa fa-tint"></span>Logos &amp; Illustrations</h2>
+		<p>I don't provide complex graphic design and illustration services. If you need a logo or graphics, I recommend you hire one of the awesome designers on <a href="http://dribbble.com">Dribbble</a>.</p>
+	</div>
+
+	<div class="srvc database">
+		<h2><span class="fa fa-database"></span>Domains, Hosting &amp; Email</h2>
+		<p>I do not provide domain names, hosting services or email accounts. I recommend <a href="http://mdtm.pl/ZIEdAF">Media Temple</a> for domains and hosting, and I recommend Gmail or Google Apps for email. If this is unfamiliar territory, I’m happy to help you through the process.</p>
+	</div>
+
+	<div class="srvc wrench">
+		<h2><span class="fa fa-wrench"></span>Some of my tools…</h2>
+		<ul>
+			<li><a href="http://getbootstrap.com/">Bootstrap</a> &amp; <a href="http://foundation.zurb.com/">Foundation</a> for layout, CSS, and responsive web design.</li>
+			<li><a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a> for icons and simple graphics.</li>
+			<li><a href="http://mbsy.co/9clQc">Dollar Photo Club</a> for royalty-free images and illustrations.</li>
+			<li><a href="http://themeforest.net/">Themeforest</a> for premium HTML5 templates and WordPress themes.</li>
+			<li><a href="http://www.woothemes.com/">WooThemes &amp; WooCommerce</a> for WordPress eCommerce.</li>
+			<li>Github for open source work like <a href="https://github.com/viljamis/ResponsiveSlides.js">Responsive Slides</a>, <a href="https://github.com/davatron5000/FitVids.js">FitVids</a>, <a href="https://github.com/Automattic/_s">Underscores</a>, <a href="https://github.com/necolas/normalize.css">Normalize</a>, and <a href="https://github.com/devinsays/options-framework-plugin">Options Framework</a>, just to name a few.</li>
+		</ul>
+	</div>
