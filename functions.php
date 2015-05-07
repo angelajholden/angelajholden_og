2db17ea6ecba38dbda36f1697a6ba0d99b6a0004
Angela J. Holden <angelajholden@gmail.com>
2015-05-07 13:40:32 -0700

    fixing an error

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ function ajh_disable_emojis() {
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	add_filter( 'tiny_mce_plugins', 'ajh_disable_emojis_tinymce' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 }
 add_action( 'init', 'ajh_disable_emojis' );
 
