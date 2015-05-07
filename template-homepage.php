b28fb18acb54f586a9eff7911fa791850d9af58c
Angela J. Holden <angelajholden@gmail.com>
2015-05-06 19:28:01 -0700

    about content update

--- template-homepage.php
+++ template-homepage.php
@@ -48,7 +48,11 @@ get_header(); the_post(); ?>
       <div class="wrap clearfix">
       	<img src="<?php bloginfo('template_url'); ?>/images/angela.jpg" alt="About Angela J. Holden Website Design" title="Angela J. Holden">
       	<h1>About Me</h1>
-      	<p>My name is Angela Holden and I’m a freelance web designer, front end developer and technical writer from Minneapolis, MN, now living in San Diego, CA. I love WordPress, content, color, typography, and providing great user experiences. I’ve been designing websites for over six years and I love every minute that I spend doing it.</p>
+      	<p>Hi, my name is Angela and I’m originally from Minneapolis, Minnesota, but now I live in San Diego, California. I moved in 2014 after I finished my Master’s Degree in Technical Communication. I also have a B.A. in Management & Employment Law, and an A.S. in Paralegal Studies. I worked for the law firm <a href="http://lawmoss.com">Moss & Barnett</a> in Minneapolis for many years as a paralegal and commercial debt collector before I started learning to code.</p>
+				
+				<p>I also have an Etsy shop called <a href="https://www.etsy.com/shop/ThemeHoney">Theme Honey</a> where I sell a premium theme I designed and developed called <a href="<?php bloginfo('uri'); ?>/crafty-theme">Crafty</a>. I do a little bit of logo design here as well, and sell Photoshop files available as a digital download.</p>
+
+				<p>This site was created as a way to show my <a href="<?php bloginfo('url'); ?>/projects">portfolio</a> and blog about WordPress and web design. I learn new things every day, and I want to share what I learn and give back to a community of designers and developers who openly share their work for people like me to learn from. I hope the work I’m doing helps someone learn to do something they love.</p>
 
         <p>I work from my home in San Diego and I have clients in the U.S. and overseas. For more information about what I can do, please <a href="#contact">contact</a> me for a complementary consultation.</p>
 
