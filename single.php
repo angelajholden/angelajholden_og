c02309a830696b56e7f0392684df3bf7d512683a
Angela J. Holden <angelajholden@gmail.com>
2015-06-14 10:50:13 -0700

    moving author-bio below sharing and tags

--- single.php
+++ single.php
@@ -55,14 +55,6 @@
 
 	    <hr>
 
-	    <figure class="gravatar"><?php echo get_avatar( get_the_author_meta('email'), '150' ); ?></figure>
-	    <div class="author-bio clearfix">
-				<h2 class="author-name"><?php the_author_posts_link(); ?></h2>
-				<p class="author-desc"><?php the_author_meta( 'description' ); ?></p>
-			</div>
-
-	    <hr>
-
 		    <?php wordpress_sharing(); ?>
 
 		    <?php if (has_tag()) : ?>
@@ -70,6 +62,14 @@
 		    <?php endif; ?>
 
 		    <?php the_post_navigation(); ?>
+
+	    <hr>
+
+	    <figure class="gravatar"><?php echo get_avatar( get_the_author_meta('email'), '150' ); ?></figure>
+	    <div class="author-bio clearfix">
+				<h2 class="author-name"><?php the_author_posts_link(); ?></h2>
+				<p class="author-desc"><?php the_author_meta( 'description' ); ?></p>
+			</div>
 
 	    <hr>
 
