a35657e3e4ae011b45e595d84a55a54a33da46c6
Angela J. Holden <angelajholden@gmail.com>
2015-08-17 21:23:20 -0700

    changing h1 to a div

--- template-homepage.php
+++ template-homepage.php
@@ -8,7 +8,7 @@ get_header(); the_post(); ?>
   			<source src="<?php echo get_template_directory_uri(); ?>/video/honeybee.mp4" type="video/mp4"></source>
   			<img src="<?php echo get_template_directory_uri(); ?>/images/honeybee.jpg" width="1600" height="900" alt="Bee in ultra slow motion By Emile van Wijk and Joris Schaap | Angela J. Holden Web Design | San Diego, CA">
   		</video>
-  		<h1 class="page-title"><?php the_title(); ?></h1>
+  		<div class="page-title"><?php the_title(); ?></div>
   		<p class="page-desc"><?php bloginfo('description'); ?></p>
   	</div>
 
