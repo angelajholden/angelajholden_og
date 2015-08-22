d98e403a425d9e0c36c945c6fdd4c8c2f48343c3
Angela J. Holden <angelajholden@gmail.com>
2015-08-22 06:21:48 -0700

    updating homepage and styles for post featured images

--- inc/coaching.php
+++ inc/coaching.php
@@ -2,8 +2,7 @@
   <div class="wrap clearfix">
     <h2>WordPress Coaching</h2>
     <p>I love teaching my clients about WordPress. Sometimes people just want to hire a developer, and sometimes they want to learn how to manage their sites. There are plenty of situations where the average user is more than capable of learning to use this software to make changes and publish new content.</p>
-    <p>In my podcast, <a href="<?php bloginfo('url'); ?>/category/wordpresscoaching/">WordPress Coaching</a>, I give users practical advice in 15 minutes or less; twice a week. Published every Monday and Thursday, I talk about blogging, content, SEO and Plugins. Listen to the first episode!</p>
+    <p>In my podcast, <a href="<?php bloginfo('url'); ?>/category/wordpresscoaching/">WordPress Coaching</a>, I give users practical advice in 15 minutes or less; twice a week. Published every Monday and Thursday, I talk about blogging, content, SEO and Plugins. Listen to the latest episode!</p>
     <hr>
-    <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/219682660&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-  </div>
+		<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/220146988&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>  </div>
 </section>
