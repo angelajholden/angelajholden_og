98aa7908b803e98875b6b405feb05fe38f5e3b2d
Angela J. Holden <angelajholden@gmail.com>
2015-08-22 06:35:46 -0700

    updating podcast verbiage

--- inc/coaching.php
+++ inc/coaching.php
@@ -1,8 +1,8 @@
 <section class="coaching">
   <div class="wrap clearfix">
     <h2>WordPress Coaching</h2>
-    <p>I love teaching my clients about WordPress. Sometimes people just want to hire a developer, and sometimes they want to learn how to manage their sites. There are plenty of situations where the average user is more than capable of learning to use this software to make changes and publish new content.</p>
-    <p>In my podcast, <a href="<?php bloginfo('url'); ?>/category/wordpresscoaching/">WordPress Coaching</a>, I give users practical advice in 15 minutes or less; twice a week. Published every Monday and Thursday, I talk about blogging, content, SEO and Plugins. Listen to the latest episode!</p>
+    <p>I love teaching my clients about WordPress. Sometimes people want to hire a developer, and sometimes they want to maintain their sites on their own. With WordPress, site owners are more than capable of learning to use this software to manage, change and publish new content.</p>
+    <p>In my podcast, <a href="<?php bloginfo('url'); ?>/category/wordpresscoaching/">WordPress Coaching</a>, I give users practical advice on one topic at a time, in 15 minutes or less. Published every Monday and Thursday, I talk about blogging, content, SEO and Plugins. Listen to the latest episode!</p>
     <hr>
 		<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/220146988&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>  </div>
 </section>
