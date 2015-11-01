9866373374184070b146f482fb2eb3db42231d4f
Angela J. Holden <angelajholden@gmail.com>
2015-11-01 09:27:02 -0800

    moving homepage banner to a template part

--- template-homepage.php
+++ template-homepage.php
@@ -3,19 +3,8 @@
 * Template Name: Homepage
 */
 get_header(); the_post(); ?>
-  	<div class="banner">
-  		<video id="bgvideo" preload="auto" muted="true" autoplay="true" loop="true" poster="<?php echo get_template_directory_uri(); ?>/images/honeybee.jpg">
-  			<source src="<?php echo get_template_directory_uri(); ?>/video/honeybee.mp4" type="video/mp4"></source>
-  			<img src="<?php echo get_template_directory_uri(); ?>/images/honeybee.jpg" width="1600" height="900" alt="Bee in ultra slow motion By Emile van Wijk and Joris Schaap">
-  		</video>
-      <div class="page-title"><?php the_title(); ?></div>
-      <p class="page-desc"><?php bloginfo('description'); ?></p>
-      <div class="overlay"></div>
-  	</div>
 
-    <section class="accepting">
-      <div class="wrap clearfix">Now accepting projects for November &amp; December 2015, January 2016<span><a href="<?php bloginfo('url'); ?>/questionnaire/">Questionnaire</a></span></div>
-    </section>
+    <?php //get_template_part('inc/banner'); ?>
 
     <?php get_template_part('inc/skills'); ?>
 
