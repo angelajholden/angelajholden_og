2c2c38b16da2dc9bf9bdc3b97448325b3b650a57
Angela J. Holden <angelajholden@gmail.com>
2015-11-21 20:55:29 -0800

    moving logos to a separate file

---
+++ inc/logos.php
@@ -0,0 +1,42 @@
+<section class="logos">
+	<div class="wrap clearfix">
+		<h2>My Clients</h2>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+		<aside class="logo-grid">
+			<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
+		</aside>
+
+	</div>
+</section>
--- template-homepage.php
+++ template-homepage.php
@@ -10,48 +10,7 @@ get_header(); the_post(); ?>
 
   	<?php get_template_part('inc/reviews'); ?>
 
-  	<section class="logos">
-  		<div class="wrap clearfix">
-  			<h2>My Clients</h2>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  			<aside class="logo-grid">
-  				<a href="http://e-mod.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/e-mod.png" draggable="false" alt="e-Modern Marketing" width="300" height="67"></a>
-  			</aside>
-
-  		</div>
-  	</section>
+    <?php //get_template_part('inc/logos'); ?>
 
     <?php get_template_part('inc/about'); ?>
 
