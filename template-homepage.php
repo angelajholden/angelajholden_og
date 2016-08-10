007da6ac59f8a6f5caf523263202f857c4613922
Angela J. Holden <angelajholden@gmail.com>
2016-08-10 06:15:58 -0500

    typo

--- template-homepage.php
+++ template-homepage.php
@@ -7,7 +7,7 @@ get_header(); the_post(); ?>
 	<section class="hero" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/bg.jpg);">
 		<div class="wrap">
 			<h1 class="headline">WordPress Web Design<br />&amp; Front End Development</h1>
-			<p class="lead">Contact me today to dicuss your project.</p>
+			<p class="lead">Contact me today to discuss your project.</p>
 			<p><a class="contact-me" href="#contact">Contact Me</a><a class="questionnaire" href="<?php bloginfo('url'); ?>/questionnaire/">Questionnaire</a></p>
 		</div>
 	</section>
