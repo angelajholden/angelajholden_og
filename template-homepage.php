3b9330d928ee48aa42e02180bd4d3334c00825ed
Angela J. Holden <angelajholden@gmail.com>
2016-08-10 12:15:13 -0500

    removing reviews and adding podcast iframe

--- template-homepage.php
+++ template-homepage.php
@@ -16,7 +16,17 @@ get_header(); the_post(); ?>
 
     <?php get_template_part('inc/skills'); ?>
 
-  	<?php get_template_part('inc/reviews'); ?>
+    <section class="reviews">
+			<div class="reviewbg">
+				<div class="wrap">
+					<h2>WordPress Coaching Podcast</h2>
+					<p class="lead">A podcast about using WordPress, and the business of WordPress.</p>
+					<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/223983376&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+			  </div>
+			</div>
+		</section>
+
+  	<?php //get_template_part('inc/reviews'); ?>
 
     <?php get_template_part('inc/about'); ?>
 
