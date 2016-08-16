4d601afb7ba0fb672401cfebbe4f3263589669e9
Angela J. Holden <angelajholden@gmail.com>
2016-08-16 10:57:11 -0500

    removing animation from hero

--- template-homepage.php
+++ template-homepage.php
@@ -5,7 +5,7 @@
 get_header(); the_post(); ?>
 
 	<section class="hero" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/bg.jpg);">
-		<div class="wrap animated fadeInDown">
+		<div class="wrap">
 			<h1 class="headline">WordPress Web Design<br />&amp; Front End Development</h1>
 			<p class="lead">Process–driven web design from start to finish.</p>
 			<p><a class="contact-me" href="#contact">Contact Me</a><a class="questionnaire" href="<?php bloginfo('url'); ?>/questionnaire/">Questionnaire</a></p>
