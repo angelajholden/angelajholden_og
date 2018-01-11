a1d1461914b3453fe6732a513d6a26be95a5d487
Angela J. Holden <angelajholden@gmail.com>
2018-01-11 13:54:47 -0600

    tidy the about content

--- inc/about.php
+++ inc/about.php
@@ -5,9 +5,9 @@
 
     <h2>About Me</h2>
 
-  	<p><span class="first-hi">Hi</span>My name is Angela Holden and I’m from Minneapolis, Minnesota. In 2013 I earned a Master’s Degree in Technical Communication with an emphasis on information architecture and usability. I also have a B.A. in Management & Employment Law, and an A.S. in Paralegal Studies. I worked for <a href="http://www.lawmoss.com/john-rossman/" target="_blank" rel="follow">John K. Rossman</a> at the law firm Moss & Barnett in Minneapolis for over five years as a paralegal, commercial debt collector and executive producer of <a href="http://johnrossmanattorney.com/" target="_blank" rel="follow">The Debt Collection Drill</a> podcast.</p>
+  	<p><span class="first-hi">Hi</span>My name is Angela Holden and I’m from Minneapolis, Minnesota. In 2013 I earned a Master’s Degree in Technical Communication with an emphasis on information architecture and usability. I also have a B.A. in Management &amp; Employment Law, and an A.S. in Paralegal Studies. I worked for <a href="http://www.lawmoss.com/john-rossman/" target="_blank" rel="follow">John K. Rossman</a> at the law firm Moss &amp; Barnett in Minneapolis for over five years as a paralegal, commercial debt collector and executive producer of <a href="http://johnrossmanattorney.com/" target="_blank" rel="follow">The Debt Collection Drill</a> podcast.</p>
 
-		<p>This website has a <a href="<?php bloginfo('url'); ?>/projects/">portfolio of the projects</a> I’ve worked on, and I'm proud of what's here. I love web design and WordPress, and I’m living the dream as a full-time front end developer. I work on a variety of projects in numerous industries, and I thoroughly enjoy the collaborative experience I have with my clients.</p>
+		<p>This website has a <a href="<?php bloginfo('url'); ?>/projects/">portfolio of the projects</a> I’ve worked on. I love HTML, CSS and JavaScript, and I’m living the dream as a frontend developer. I work on a variety of projects in numerous industries, and I thoroughly enjoy the collaborative experience I have with my clients.</p>
 
     <p>For more information about what I can do, please <a href="#contact">contact</a> me for a consultation. You can also review my <a href="<?php bloginfo('url') ?>/terms-conditions/">project terms</a>.</p>
 
