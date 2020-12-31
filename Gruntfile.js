f402fdcab8e5b9cc802cb529ad85b14c38bf01c2
Angela J. Holden <angelajholden@gmail.com>
2020-12-31 14:37:04 -0600

    cleanup theme files and copyedit readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,76 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 		/**
-		* Sass Task
-		*/
+		 * Sass Task
+		 */
 		sass: {
 			dist: {
 				options: {
-					style: 'compressed',
-					sourcemap: 'auto',
+					style: "compressed",
+					// sourcemap: 'auto',
 				},
 				files: {
-					'compiled/style.css': 'scss/style.scss'
-				}
-			}
+					"compiled/style.css": "scss/style.scss",
+				},
+			},
 		},
 		/**
-		* Compile JS files
-		*/
-	    concat: {
-	      options: {
-	        stripBanners: true
-	      },
-	      js : {
-	        files: {
-	          'compiled/base.js':
-	          [
-	            'js/*.js',
-	          ]
-	        }
-	      }
-	    },
+		 * Compile JS files
+		 */
+		concat: {
+			options: {
+				stripBanners: true,
+			},
+			js: {
+				files: {
+					"compiled/base.js": ["js/*.js"],
+				},
+			},
+		},
 		/**
-		* Minify compiled JS file
-		*/
-	    uglify : {
-	      js: {
-	        files: {
-	          'compiled/base.min.js': ['compiled/base.js'],
-	        }
-	      }
-	    },
+		 * Minify compiled JS file
+		 */
+		uglify: {
+			js: {
+				files: {
+					"compiled/base.min.js": ["compiled/base.js"],
+				},
+			},
+		},
 		/**
-		* Autoprefixer
-		*/
+		 * Autoprefixer
+		 */
 		autoprefixer: {
 			options: {
-				browsers: ['last 2 versions'],
+				browsers: ["last 2 versions"],
 				map: true,
 			},
 			multiple_files: {
 				expand: true,
 				flatten: true,
-				src: 'compiled/*.css',
-				dest: '',
-			}
+				src: "compiled/*.css",
+				dest: "",
+			},
 		},
 		/**
-		* Watch Task
-		*/
+		 * Watch Task
+		 */
 		watch: {
 			css: {
-				files: '**/*.scss',
-				tasks: ['sass', 'autoprefixer']
+				files: "**/*.scss",
+				tasks: ["sass", "autoprefixer"],
 			},
 			js: {
-				files: 'js/*.js',
-				tasks: ['concat', 'uglify']
-			}
-		}
+				files: "js/*.js",
+				tasks: ["concat", "uglify"],
+			},
+		},
 	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['watch']);
-}
+	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-autoprefixer");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.registerTask("default", ["watch"]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-#Angela J. Holden
-Custom WordPress Theme
+# Angela J. Holden :maple_leaf:
+
+## A custom WordPress theme and portfolio.
+
+This is the third version of my first website.
+
+### November 21, 2013
+
+I made my first commit in 2013 before I even had my first job as a developer.
+
+![Initial Git Commit](/images/initial-commit.png)
+
+---
+
+### Many Iterations
+
+This is a custom WordPress theme that has been developed six-ways-from-Sunday over the last seven years. It is available for fork or clone for anyone who wants to use it.
+
+```bash
+git clone git@github.com:angelajholden/angelajholden_v3.git
+```
+
+Cheers! :two_hearts:  
+Angela
