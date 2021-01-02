5054292fc87ba0b0fa5d681c6dbfb827e4291101
Angela J. Holden <angelajholden@gmail.com>
2021-01-02 10:05:55 -0600

    working on the homepage

--- template-homepage.php
+++ template-homepage.php
@@ -4,28 +4,25 @@
 */
 get_header(); the_post(); ?>
 
-	<section class="hero">
-		<div class="wrap">
-			<h1 class="headline">Frontend Developer<br />&amp; UI/UX Designer</h1>
-			<p><a class="contact-me" href="#contact">Contact Me</a><a class="questionnaire" href="<?php bloginfo('url'); ?>/projects/">View Projects</a></p>
-		</div>
-	</section>
+<section class="hero">
+    <div class="wrap">
+        <h1 class="headline">Frontend &amp;<br>UI Development</h1>
+        <p><a class="questionnaire" href="<?php bloginfo('url'); ?>/projects/">View Projects</a></p>
+    </div>
+</section>
 
-    <?php //get_template_part('inc/banner'); ?>
+<?php get_template_part('inc/skills'); ?>
 
-    <?php get_template_part('inc/skills'); ?>
+<section class="reviews">
 
-    <section class="reviews">
-			<div class="reviewbg">
-				<div class="wrap">
-					<h2>Frontend Development</h2>
-					<p class="lead">I'm available for UI/UX design and frontend development.<br />I can create the design and develop the frontend code for any project.</p>
-			  </div>
-			</div>
-		</section>
+    <div class="wrap">
+        <h2>Frontend &amp; UI Developer</h2>
+        <p class="lead">My expertise is HTML5, SCSS and JavaScript. I build websites and web apps with WordPress, Vue,
+            Node and Express.</p>
+    </div>
 
-  	<?php //get_template_part('inc/reviews'); ?>
+</section>
 
-    <?php get_template_part('inc/about'); ?>
+<?php get_template_part('inc/about'); ?>
 
 <?php get_footer(); ?>
